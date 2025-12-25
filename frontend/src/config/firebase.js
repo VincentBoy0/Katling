f5433f29e56cd1a889adcc3ad63fba5d3c07f235
Dang Thanh Long <dangthanhlong.ltv@gmail.com>
2025-12-25 16:24:11 +0700

    update config

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,3 +1,9 @@
 export const firebaseConfig = {
-  
+  apiKey: "AIzaSyA7Uy7k0f6SR9lpyglSV0RM2AFRsjAKDaE",
+  authDomain: "testing-fastapi-auth.firebaseapp.com",
+  projectId: "testing-fastapi-auth",
+  storageBucket: "testing-fastapi-auth.firebasestorage.app",
+  messagingSenderId: "335238473805",
+  appId: "1:335238473805:web:b6bf2c0877dfb3e6e4d404",
+  measurementId: "G-TLTJ7N16CR"
 };
